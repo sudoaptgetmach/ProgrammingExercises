7825f00ece7ce18e006d0cf3a110571c5921105e
Luiz <qluizmachado@gmail.com>
2024-12-13 04:55:15 -0300

    Add Palindrome checker implementation
    Took 13 minutes

---
+++ java/src/leetcode/Palindrome.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Palindrome {
+
+    public boolean isPalindrome(int x) {
+
+        String str = Integer.toString(x);
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
