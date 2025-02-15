4d682fcd52139d5024b981ee2ff95be6dff97c77
Luiz <qluizmachado@gmail.com>
2025-02-15 15:18:34 -0300

    Update exercicio01.c

--- c/unaerp/matrizes/exercicio01/exercicio01.c
+++ c/unaerp/matrizes/exercicio01/exercicio01.c
@@ -5,21 +5,19 @@
 
 int main() {
 	
-	int matriz[3][3];
+    int matriz[3][3];
 	
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; i < 3; j++) {
-			matriz[i][j] = i + j;
-		}	
-	}
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; i < 3; j++) {
+            matriz[i][j] = i + j;
+        }	
+    }
 	
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) { 
             printf("%d ", matriz[i][j]); 
         }
         printf("\n"); 
-    }
-	
-	
-	return 0;
+    }	
+    return 0;
 }
