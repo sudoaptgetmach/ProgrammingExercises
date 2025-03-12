492332cfc2dc28c1f5cd1e532800c5760891373b
Luiz <qluizmachado@gmail.com>
2025-03-12 20:09:06 -0300

    Add exercicio04.cpp for comparing four numbers and create project configuration files
    Took 19 seconds

---
+++ c/unaerp/algoritmos/aula/exercicio04/exercicio04.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, d, max, min;
+
+    printf("Digite o primeiro valor a ser comparado: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo valor a ser comparado: ");
+    scanf("%d", &b);
+    
+    printf("Digite o terceiro valor a ser comparado: ");
+    scanf("%d", &c);
+
+    printf("Digite o quarto valor a ser comparado: ");
+    scanf("%d", &d);
+
+    max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+
+
+    min = a;
+    if (min > b) min = b;
+    if (min > c) min = c;
+    if (min > d) min = d;
+
+    printf("O maior número é: %d", max);
+    printf("O menor número é: %d", min);
+
+    return 0;
+}
