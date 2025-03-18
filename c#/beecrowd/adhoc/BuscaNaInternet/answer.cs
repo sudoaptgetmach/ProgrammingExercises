23b50a06a425dc8be7e12030e4dabfb5fb08da1b
Luiz <qluizmachado@gmail.com>
2025-03-18 01:56:54 -0300

    Create answer.cs

---
+++ c#/beecrowd/adhoc/BuscaNaInternet/answer.cs
@@ -0,0 +1,10 @@
+using System; 
+
+class URI {
+    static void Main(string[] args) { 
+        int n;
+        n = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(n*4);
+    }
+}
