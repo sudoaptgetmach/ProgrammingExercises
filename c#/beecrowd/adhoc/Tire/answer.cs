d401a20abc7dad1ed29c49cd1cf22fcc36499cc2
Luiz <qluizmachado@gmail.com>
2025-03-18 01:58:47 -0300

    Create answer.cs

---
+++ c#/beecrowd/adhoc/Tire/answer.cs
@@ -0,0 +1,11 @@
+using System; 
+
+class URI {
+    static void Main(string[] args) { 
+        int n, c;
+        n = Convert.ToInt32(Console.ReadLine());
+        c = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(n-c);
+    }
+}
