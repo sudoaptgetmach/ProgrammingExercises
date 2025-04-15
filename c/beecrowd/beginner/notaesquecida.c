9c45ddfa55f1a83effa1d87c0f4ae82ec533df92
Luiz <qluizmachado@gmail.com>
2025-04-15 05:13:04 -0300

    Add notaesquecida.c to calculate media based on user input
    Took 4 minutes

---
+++ c/beecrowd/beginner/notaesquecida.c
@@ -0,0 +1,18 @@
+//
+// Created by mach on 15/04/25.
+//
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    int media = (y*2)-x;
+
+    printf("%d\n", media);
+
+    return 0;
+}
